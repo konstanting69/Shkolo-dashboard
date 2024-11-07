31d11e0558a9f45c17b6a4129f9afd9e6221e568
konstanting69 <45396105+konstanting69@users.noreply.github.com>
2024-11-07 13:54:55 +0000

    Changed css call to secure_asset

--- shkolo-dashboard/resources/views/cells/index.blade.php
+++ shkolo-dashboard/resources/views/cells/index.blade.php
@@ -4,7 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cells Dashboard</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    
+    <link rel="stylesheet" href="{{ secure_asset('css/app.css') }}">
+
     
 </head>
 <body>
